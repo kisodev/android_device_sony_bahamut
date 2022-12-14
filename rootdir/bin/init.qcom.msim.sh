ee6b180553db7775c31903a6ebf5af7f2698f967
soralis0912 <soralis.nem@gmail.com>
2022-12-14 09:38:55 +0900

    fix check dual sim

---
+++ rootdir/bin/init.qcom.msim.sh
@@ -0,0 +1,19 @@
+#!/vendor/bin/sh
+
+MSIM_DEVICES=(
+    j9210 j9260 # Dual-SIM version
+)
+MSIM_DEVICE=0
+
+for device in "${MSIM_DEVICES[@]}"; do
+    if grep -qi "Model: ${device}" /dev/block/bootdevice/by-name/LTALabel; then
+        MSIM_DEVICE=1
+        break
+    fi
+done
+
+if [[ "${MSIM_DEVICE}" -eq 1 ]]; then
+    setprop vendor.radio.multisim.config dsds
+else
+    setprop vendor.radio.multisim.config ss
+fi
