3404bb0573d5fdc924caf56c3a95be4d421900ef
soralis0912 <soralis.nem@gmail.com>
2023-01-20 21:00:21 +0900

    add compile

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -7,3 +7,11 @@ LOCAL_MODULE := lib-imsvtshim
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := lib-camshim.cpp
+LOCAL_MODULE := lib-camshim
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
