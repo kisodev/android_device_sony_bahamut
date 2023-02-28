b238ee4566c9513582ed8942facf50f000a693bc
soralis0912 <soralis.nem@gmail.com>
2023-03-01 02:00:59 +0900

    add config

--- device.mk
+++ device.mk
@@ -33,7 +33,12 @@ TARGET_SCREEN_WIDTH := 1440
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 28
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/sony/bahamut/bahamut-vendor.mk)
