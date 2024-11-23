f13df001295b70f16bebb32cfab9c5880e34b254
voidanix <voidanix@keyedlimepie.org>
2024-11-24 00:25:49 +0100

    bahamut: Use python extract-utils
    Signed-off-by: voidanix <voidanix@keyedlimepie.org>

---
+++ extract-files.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/sony/sm8150-common',
+    'hardware/qcom-caf/wlan',
+]
+
+module = ExtractUtilsModule(
+    'bahamut',
+    'sony',
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8150-common', module.vendor
+    )
+    utils.run()
